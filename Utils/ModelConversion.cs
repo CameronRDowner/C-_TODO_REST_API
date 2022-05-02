c4483218a37c1d9bcb7180d26b375dd366d30811
Cameron Downer <camerondowner@gmail.com>
2022-05-02 18:56:41 -0500

    added controller for handling todo item requests

---
+++ Utils/ModelConversion.cs
@@ -0,0 +1,12 @@
+﻿using TodoApi.Models;
+
+namespace TodoApi.Utils
+{
+    public class ModelConversion
+    {
+        public TodoItemDTO ToDto(TodoItem todoItem)
+        {
+            return new TodoItemDTO { Id = todoItem.Id, Name = todoItem.Name, IsComplete = todoItem.IsComplete};
+        }
+    }
+}
